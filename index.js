0225b9f7b957c2e647d404c4e620f9ffaa0e80a3
dazy12 <116194555+dazy12@users.noreply.github.com>
2023-10-15 18:29:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const click_menu=document.getElementById("bar-btn");
+const click_close=document.getElementById("close-btn");
+const mybar=document.getElementById("c");
+
+click_menu.addEventListener("click",function(){
+    mybar.classList.toggle("active");
+})
+
+click_close.addEventListener("click",function(){
+    mybar.classList.remove("active");
+})
+
+// home section
+const homes=document.getElementById("home");
+const about=document.getElementById("about");
+const achiev=document.getElementById("achiev");
+const contact=document.getElementById("contact");
+
+about.addEventListener("click",function(){
+  
+})
+
